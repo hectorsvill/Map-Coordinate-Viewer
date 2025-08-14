67a7d7a1c9e7e752f42c491be7549a52f303fb1d
Hector Steven Villasano <hectorstevenvillasano@HectorGs-MacBook-Pro.local>
2025-08-14 14:38:57 -0400

    integrated leflet map

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MapComponent from './components/MapComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const defaultLatitude = 48.8584;
+  const defaultLongitude = 2.2945;
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-screen w-screen flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Map Coordinate Viewer</h1>
+      <MapComponent latitude={defaultLatitude} longitude={defaultLongitude} />
+    </div>
   )
 }
 
